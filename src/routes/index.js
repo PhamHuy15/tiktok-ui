0f92e155d0ab885394aa3b07ec69e0ab25a87e58
PhamHuy15 <ahihi01152004@gmail.com>
2024-09-06 22:20:40 +0700

    Cấu hình Router/Layout

---
+++ src/routes/index.js
@@ -0,0 +1,37 @@
+import { HeaderOnly } from '~/layouts';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//publickRoute
+const publickRoute = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+];
+
+const privateRouter = [];
+
+export { publickRoute, privateRouter };
